bfc2635321ab15aabb097d1132ea2cf1ac5f87f3
Nicholas Knueppel <nick@knueppel.com>
2026-07-06 12:38:16 -0700

    Test of basic macro

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stddef.h>
 
+// Test of standard macro (NUMBER)
+// to test the effect, use -E option of gcc
+#define NUMBER = 216
+
 int main(int argc, const char* arvg[]) {
+  int num = NUMBER;
   return 0;
 }
